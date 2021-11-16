e399cd77a6002823211bff3c496d89294e147e91
fernandopino9 <93406375+fernandopino9@users.noreply.github.com>
2021-11-16 21:14:07 +0200

    Add files via upload

---
+++ Program pg (1).cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Please enter temperature in Fahrenheit:); double tempF = double.Parse(Console.ReadLine()); Console.WriteLine("Please enter temperature in Celsius:"); double tempC = double.Parse(Console.ReadLine()); double fahrenheitTocelsius = (tempF - 32) * 5 / 9; double celsiusTofahrenheit = (tempC * 9 / 5) + 32;
+
+            Console.WriteLine("The temperature converted from Fahrenheit to Celsius is:" +
+                "+ fahrenheitTocelsius)
+                Console, WriteLine(The temperature converted from Celsius to Fahrenheit is:
+                    "+ celsiusTofahrenheit)                                        
+
+
+        }
+    }
+}
