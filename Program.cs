ea70a199a789ce7e4334ac5265071ae4f9e206a3
fernandopino9 <93406375+fernandopino9@users.noreply.github.com>
2022-01-05 22:57:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isOk = IsValidEmail("mma @gma.com");
+            Console.WriteLine(isOk);
+        }
+        static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
