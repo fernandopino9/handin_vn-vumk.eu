6ede8817fe5174b46fe995e88cfbf4ad2e7c85fa
fernandopino9 <93406375+fernandopino9@users.noreply.github.com>
2022-01-22 18:31:20 +0200

    Add files via upload

---
+++ 3.2 assigment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+            using System.Linq;
+
+namespace Assignment3RemoveDuplicatesfromArray
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int[] array = { 1, 2, 2, 3, 3, 4, 1, 2, 5 };
+
+                // function to remove the duplicates
+                int[] distinct = array.Distinct().ToArray();
+
+                Console.WriteLine("The array without duplicates is: " + String.Join(",", distinct));
+            }
+    }
+}
